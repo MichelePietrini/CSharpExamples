49f0e59c0afd29d8e6f0be5c38e39fcaac1701e8
MichelePietrini <michele.pietrini@gmail.com>
2024-08-27 18:18:00 +0200

    Init: Initialized String Manipulator MVC project

---
+++ StringManipulator/StringManipulator/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StringManipulator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
