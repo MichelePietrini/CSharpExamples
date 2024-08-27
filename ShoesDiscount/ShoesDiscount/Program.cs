10dcda8f2694d1409c2b934638f71cd8af5573bc
MichelePietrini <michele.pietrini@gmail.com>
2024-08-27 18:12:52 +0200

    Init: Initialized Shoes Discount project with basic functionality

---
+++ ShoesDiscount/ShoesDiscount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ShoesDiscount
+{
+    class Program
+    {
+        class Product
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+            public override string ToString() => $"{Name} {Price}";
+        }
+
+        static void ApplyDiscount(Product shoes)
+        {
+            shoes = new Product { Name = "discounted shoes", Price = shoes.Price / 2 };
+        }
+
+        static void Main(string[] args)
+        {
+            var shoes = new Product { Name = "my shoes", Price = 100 };
+            ApplyDiscount(shoes);
+            Console.WriteLine(shoes); // Output: my shoes 100
+        }
+    }
+}
